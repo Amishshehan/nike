aa5f13933f488767708df492d63e7ca09cb1878a
Amishshehan <amishshehan@gmail.com>
2023-09-29 03:41:27 +0530

    developed subscribe section

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,4 +1,11 @@
-const Button = ({ label, iconURL, backgroundColor, borderColor, textColor,fullWidth }) => {
+const Button = ({
+  label,
+  iconURL,
+  backgroundColor,
+  textColor,
+  borderColor,
+  fullWidth,
+}) => {
   return (
     <button
       className={`flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none
@@ -10,13 +17,15 @@ const Button = ({ label, iconURL, backgroundColor, borderColor, textColor,fullWi
     >
       {label}
 
-      {iconURL && <img
-        src={iconURL}
-        alt="Arrow Right Icon"
-        className="rounded-full w-5 h-5 ml-2"
-      />}
+      {iconURL && (
+        <img
+          src={iconURL}
+          alt='arrow right icon'
+          className='ml-2 rounded-full bg-white w-5 h-5'
+        />
+      )}
     </button>
-  )
-}
+  );
+};
 
-export default Button
+export default Button;
